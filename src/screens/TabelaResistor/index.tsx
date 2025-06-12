263e68a5674dbdf0d6eb883e4e14c01a5a615f29
Hevellyn Mesquita <109560035+hevellyn16@users.noreply.github.com>
2025-06-12 20:05:47 -0300

    feat: Add TabelaResistor Title

--- src/screens/TabelaResistor/index.tsx
+++ src/screens/TabelaResistor/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {
     Text,
-
+    View,
+    SafeAreaView,
+    TouchableOpacity,
 } from 'react-native';
 
 export default function TabelaResistor() {
     return (
-        <>
-            <Text>Tabela de Resistores</Text>
-            {/* Aqui você pode adicionar a tabela de resistores */}
-        </>
+        <SafeAreaView className="flex flex-col justify-between items-center w-full h-full py-12 px-7 bg-white">
+                    <View className="flex justify-center items-center w-full gap-5 py-5 px-0 rounded-lg bg-white shadow-md shadow-black/50">
+                        <Text className="text-5xl font-bold text-gray-800">Tabela de Resistência</Text>
+                    </View>
+        
+                
+                </SafeAreaView>
     );
 }
